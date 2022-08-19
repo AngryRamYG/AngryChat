256e07ad987b01e9909f0e19761dee5e64538c4e
AngryRamYG <RamiGerges@angrymonkeyagency.com>
2022-08-19 17:06:33 +0300

    Show Connected IDs and chat

--- AngryChat/Hubs/ChatHub.cs
+++ AngryChat/Hubs/ChatHub.cs
@@ -4,9 +4,26 @@ namespace BlazorServerSignalRApp.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message, string recieverid, string senderid)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message, recieverid, senderid);
         }
+
+
+        public override Task OnConnectedAsync()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
     }
 }
